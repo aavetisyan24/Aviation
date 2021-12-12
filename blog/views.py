d4123e76c569328de9c48ac236132ae641b63994
Artak Avetisyan <av.artak24@gmail.com>
2021-12-12 23:10:41 +0400

    Django first project

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# posts = [
+#     {
+#         "author": "Artak Avetisyan",
+#         "title": "Blog Post1",
+#         "content": "First Post",
+#         "date_posted": "30 November 2021"
+#     },
+#     {
+#         "author": "Hasmik Amiryan",
+#         "title": "Blog Post2",
+#         "content": "Second Post",
+#         "date_posted": "29 November 2021"
+#     }
+# ]
+
+
+def home(request):
+    context = {
+       "posts": Post.objects.all()
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
+
+
+def flyto(request):
+    return render(request, "blog/flyto.html", {"title": "Fly To"})
+
